924936c24d03a0b7e76e21f25f270af5d06cbabf
sroballos <sroballos@udesa.edu.ar>
2024-09-09 14:22:26 -0300

    Commit Inicial

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+import Main from "./pages/Main";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <body>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route component={NotFound} />
+      </Switch>
+    </body>
+  </BrowserRouter>
+);
