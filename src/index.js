3deb8deaf0d462ae7115630a0758377df2712e5d
marcosbeccar <beccarvarelam@udesa.edu.ar>
2024-09-11 22:31:19 -0300

    agarrense que llegue

--- src/index.js
+++ src/index.js
@@ -16,4 +16,4 @@ root.render(
         <Route component={NotFound} />
       </Switch>
   </BrowserRouter>
-);
+); 
--- src/pages/movieGrid/MovieGrid.js
+++ src/pages/movieGrid/MovieGrid.js
@@ -8,7 +8,7 @@ class MovieGrid extends Component {
         super()
         this.setState = {
             test: 0
-        }
+        };
     }
     render() {
         return (
