a9ce3e56ecd9ab975e7a08d616c800999bbaf4aa
marcosbeccar <beccarvarelam@udesa.edu.ar>
2024-09-14 17:39:50 -0300

    terminé el detail movie, faltaría poder pasarle un id específico

--- src/components/MovieDetailInfo/MovieDetailInfo.js
+++ src/components/MovieDetailInfo/MovieDetailInfo.js
@@ -1,40 +1,62 @@
 import { Component } from "react";
-import {options} from '../../options'
+import { options } from "../../options";
 
-//import "./.css"
+import "./MovieDetailInfo.css";
 
 class MovieDetail extends Component {
-    constructor() {
-        super()
-        this.state = {
-            data: []
-        }
+  constructor() {
+    super();
+    this.state = {
+      data: [],
     };
+  }
 
-    componentDidMount(){
-        fetch('https://api.themoviedb.org/3/movie/popular?language=es&page=1', options)
-        .then(response => response.json())
-        .then(data => {console.log(data); this.setState({data:data.results})})
-        .catch(err => console.error(err));
-    }
+  componentDidMount() {
+    fetch("https://api.themoviedb.org/3/movie/134?language=en-US", options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ data: data });
+      })
+      .catch((err) => console.error(err));
+  }
 
-    render() {
-        return (
-            <>
-            <h2>Hola, soy el MovieDetail</h2>
-            {this.state.data.length > 0 ? (
-                <>
-                    <h3>{this.state.data[4].title}</h3> 
-                    <img src={`https://image.tmdb.org/t/p/w300${this.state.data[4].poster_path}`} alt='no'/>
-                </>
-                ) : (
-                    <h3>Cargando...</h3>  
-                )}
+  render() {
+    return (
+      <>
+        {this.state.data && this.state.data.genres ? (
+          <div className="movie-detail">
+            <div className="movie-image">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${this.state.data.backdrop_path}`}
+                alt="Backdrop"
+              />
+            </div>
+            <div className="movie-info">
+              <h1>{this.state.data.title}</h1>
+              <p>Rating: {this.state.data.vote_average}</p>
 
+              <div className="genres">
+                <p>Géneros: </p>
+                {this.state.data.genres.map((genero, idx) => (
+                  <p key={idx} className="genre-item">
+                    {genero.name}
+                  </p>
+                ))}
+              </div>
 
-            </>
-        );
-    }
+              <button>Agregar a Favoritos</button>
+              <p>Fecha de estreno: {this.state.data.release_date}</p>
+              <p>Duración en minutos: {this.state.data.runtime}</p>
+              <h4>{this.state.data.overview}</h4>
+            </div>
+          </div>
+        ) : (
+          <h3>Cargando...</h3>
+        )}
+      </>
+    );
+  }
 }
 
 export default MovieDetail;
