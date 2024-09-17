e4b75ab747853686790bdc4fea5650321251839b
marcosbeccar <beccarvarelam@udesa.edu.ar>
2024-09-17 15:08:29 -0300

    agregué el boton de agregar favoritos a la pagina de detalle + adaptación

--- src/components/MovieDetailInfo/MovieDetailInfo.js
+++ src/components/MovieDetailInfo/MovieDetailInfo.js
@@ -9,8 +9,29 @@ class MovieDetail extends Component {
     super(props);
     this.state = {
       data: [],
+      esFavorito: false
     };
   }
+  
+  //misma lógica que en Movie para agregar una pelicula a favoritos
+  agregarFavorito() {
+    
+    const storage = localStorage.getItem('favoritos');
+    if (storage !== null) {
+      const parsedArray = JSON.parse(storage); // ahora está en formato JS
+      parsedArray.push(Number(this.props.id)); // con esto lo guardamos, por alguna razón era un string y lo convertí a num
+      const stringArray = JSON.stringify(parsedArray); // lo convierte
+      localStorage.setItem('favoritos', stringArray); // le paso el nombre del dato que quiero guardar, y el dato
+    } else {
+      const primerMovie = [Number(this.props.id)];
+      const stringArray = JSON.stringify(primerMovie); // lo convierte
+      localStorage.setItem('favoritos', stringArray); // le paso el nombre del dato que quiero guardar, y el dato
+    }
+    this.setState({
+      esFavorito: true
+    });
+  }
+
 
   
 
@@ -21,10 +42,32 @@ class MovieDetail extends Component {
     fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         this.setState({ data: data });
       })
       .catch((err) => console.error(err));
+
+
+    //fav
+    const storage = localStorage.getItem('favoritos');
+    if (storage !== null) {
+      const parsedArray = JSON.parse(storage);
+      const estaEnFavoritos = parsedArray.includes(Number(this.props.id)); // esto devuelve true o false
+      this.setState({
+        esFavorito: estaEnFavoritos // true o false
+      });
+    }
+  }
+
+  sacarFavorito() {
+    const storage = localStorage.getItem('favoritos');
+    const parsedArray = JSON.parse(storage); // ahora está en formato JS
+    // tengo que ver si tengo el id de una película para ver si está o no en favoritos
+    const favoritosRestantes = parsedArray.filter(id => id !== Number(this.props.id));
+    const stringArray = JSON.stringify(favoritosRestantes); // lo convierte
+    localStorage.setItem('favoritos', stringArray); // le paso el nombre del dato que quiero guardar, y el dato
+    this.setState({
+      esFavorito: false
+    });
   }
 
   render() {
@@ -54,7 +97,13 @@ class MovieDetail extends Component {
 
               <p>Fecha de estreno: {this.state.data.release_date}</p>
               <p>Duración en minutos: {this.state.data.runtime}</p>
-              <button>Agregar a Favoritos</button>
+              
+              <button onClick={() =>
+              this.state.esFavorito ? this.sacarFavorito() : this.agregarFavorito()
+              }>
+              {!this.state.esFavorito ? 'Agregar a Favoritos' : 'Sacar de Favoritos'}
+              </button>
+
               <h4>{this.state.data.overview}</h4>
             </div>
           </div>
