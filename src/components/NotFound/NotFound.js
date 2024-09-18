5318b666693f85cbb417ac0fa666a8c28b6fc7f2
CarolinaMartinezz <cbmartinez@udesa.edul.ar>
2024-09-18 12:18:38 -0300

    not found gif

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,13 +5,17 @@ import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div className="not-found-container">
+    <div className='megaContainer'>
+    <img src='../../../images/homerGif.webp' alt='gif' className='homeroGif' />
+    <div className="not-found-container">
         <h1 className="not-found-titulo">404</h1>
         <h2 className="not-found-subtitulo">Opps! Página no encontrada</h2>
         <p className="not-found-texto">
             Estas buscando una página que no existe.
         </p>
         <Link to="/" className="not-found-home-link">Volve al inicio</Link>
+    </div>
+    <img src='../../../images/homerGif.webp' alt='gif' className='homeroGif' />
     </div>
     );
   }
