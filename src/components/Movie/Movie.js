1dda4fcb8cb8329cadceb8841544e93fba2c22c5
sroballos <sroballos@udesa.edu.ar>
2024-09-23 17:34:10 -0300

    Stateful components

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -6,7 +6,7 @@ import "./Movie.css";
 
 class Movie extends Component {
   constructor({ title, desc, img, id }) {
-    super();
+    super(title, desc, img, id);
     this.state = {
       title: title,
       desc: desc,
@@ -18,42 +18,42 @@ class Movie extends Component {
   }
 
   agregarFavorito() {
-    const storage = localStorage.getItem('favoritos');
+    const storage = localStorage.getItem("favoritos");
     if (storage !== null) {
       const parsedArray = JSON.parse(storage); // ahora está en formato JS
       parsedArray.push(this.props.id); // con esto lo guardamos
       const stringArray = JSON.stringify(parsedArray); // lo convierte
-      localStorage.setItem('favoritos', stringArray); // le paso el nombre del dato que quiero guardar, y el dato
+      localStorage.setItem("favoritos", stringArray); // le paso el nombre del dato que quiero guardar, y el dato
     } else {
       const primerMovie = [this.props.id];
       const stringArray = JSON.stringify(primerMovie); // lo convierte
-      localStorage.setItem('favoritos', stringArray); // le paso el nombre del dato que quiero guardar, y el dato
+      localStorage.setItem("favoritos", stringArray); // le paso el nombre del dato que quiero guardar, y el dato
     }
     this.setState({
-      esFavorito: true
+      esFavorito: true,
     });
   }
 
   componentDidMount() {
-    const storage = localStorage.getItem('favoritos');
+    const storage = localStorage.getItem("favoritos");
     if (storage !== null) {
       const parsedArray = JSON.parse(storage);
       const estaEnFavoritos = parsedArray.includes(this.props.id); // esto devuelve true o false
       this.setState({
-        esFavorito: estaEnFavoritos // true o false
+        esFavorito: estaEnFavoritos, // true o false
       });
     }
   }
 
   sacarFavorito() {
-    const storage = localStorage.getItem('favoritos');
+    const storage = localStorage.getItem("favoritos");
     const parsedArray = JSON.parse(storage); // ahora está en formato JS
     // tengo que ver si tengo el id de una película para ver si está o no en favoritos
-    const favoritosRestantes = parsedArray.filter(id => id !== this.props.id);
+    const favoritosRestantes = parsedArray.filter((id) => id !== this.props.id);
     const stringArray = JSON.stringify(favoritosRestantes); // lo convierte
-    localStorage.setItem('favoritos', stringArray); // le paso el nombre del dato que quiero guardar, y el dato
+    localStorage.setItem("favoritos", stringArray); // le paso el nombre del dato que quiero guardar, y el dato
     this.setState({
-      esFavorito: false
+      esFavorito: false,
     });
   }
 
@@ -79,7 +79,9 @@ class Movie extends Component {
             ></img>
             <h2 className="title">{this.state.title}</h2>
             {this.state.hidden ? (
-              <button onClick={() => this.hideHandler()}>Ver Descripción</button>
+              <button onClick={() => this.hideHandler()}>
+                Ver Descripción
+              </button>
             ) : (
               <>
                 <button onClick={() => this.hideHandler()}>
@@ -88,13 +90,22 @@ class Movie extends Component {
                 <p>{this.state.desc}</p>
               </>
             )}
-            <Link className='esteLinkMolestaAlCss' to={`/detail/${this.state.id}`}>
+            <Link
+              className="esteLinkMolestaAlCss"
+              to={`/detail/${this.state.id}`}
+            >
               <button>Ir a Detalle</button>
             </Link>
-            <button onClick={() =>
-              this.state.esFavorito ? this.sacarFavorito() : this.agregarFavorito()
-            }>
-              {!this.state.esFavorito ? 'Agregar a Favoritos' : 'Sacar de Favoritos'}
+            <button
+              onClick={() =>
+                this.state.esFavorito
+                  ? this.sacarFavorito()
+                  : this.agregarFavorito()
+              }
+            >
+              {!this.state.esFavorito
+                ? "Agregar a Favoritos"
+                : "Sacar de Favoritos"}
             </button>
           </section>
         )}
--- src/pages/SeeAll/Best.js
+++ src/pages/SeeAll/Best.js
@@ -1,12 +1,29 @@
+import {Component} from "react";
 import React from "react";
 import MovieGrid from "../../components/MovieGrid/MovieGrid";
 
-const Best = () => {
-    return (
-        <>
-            <MovieGrid url="https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1" limit = "30" />
-        </>
-    );
+export class Best extends Component {
+    
+    constructor(props){
+        super(props)
+        this.state = {
+            limit: 6
+        }
+    }
+
+    verMas(){
+        this.setState({
+            limit: this.state.limit + 3,
+        });
+    }
+
+    render() {
+        return (
+            <>
+            {this.state.limit % 2 === 0 ? <><h1>Viendo {this.state.limit} películas</h1><button onClick={() => this.verMas()}>Ver Más</button><MovieGrid url={`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`} limit = {this.state.limit} /> </> : <><h1>Viendo {this.state.limit} películas impares</h1><button onClick={() => this.verMas()}>Ver Más</button><MovieGrid url={`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`} limit = {this.state.limit + 3} /></>}
+            </>
+        )
+    }
 }
 
-export default Best;
+export default Best
